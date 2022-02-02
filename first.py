2fbdc84d9f4e6240b92be077f0582e822890577e
md <mdsolanki8407@gmail.com>
2022-02-02 22:55:51 +0530

    first python

---
+++ first.py
@@ -0,0 +1,61 @@
+#single line comment
+
+"""
+    multiline comment..
+"""
+
+'''
+    this is also multiline comment
+'''
+# print("helloo")
+
+# print('hello')
+
+
+# print("mayur solanki" ,"9989848" "hii/")
+
+
+
+"""
+
+"""
+
+# print('the location of python interpreter : "c:\\new foldr\\temp\\python\\"')
+
+
+# print("python\b\b\broyal")
+# print("python\rRoyal")
+
+
+
+# print('in python ,we use "\\t" to give tab. But if you want to print "\\t" then you have ti type "\\\\t"')
+
+
+# write a python program that takes 5 integer (a,b,c,d, and e ) from user, finds their aṇverage and print the final out exactly as below
+
+# sample Execution
+#ṇ enter five integerr 
+
+
+
+
+
+# f---string
+
+
+
+
+
+# print("enter tṇhe five number:")
+
+# int(input()ṇ)
+
+
+
+
+s = "this batch is going to rock the software industry. "
+
+
+a2=s[13:]
+
+print(a2)
